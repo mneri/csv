f99246ea6107d8d6fc8b6f2c251c50161d451063
Massimo Neri <hello@mneri.me>
2017-07-09 14:19:12 +0200

    New test

--- src/test/java/me/mneri/csv/test/MainTest.java
+++ src/test/java/me/mneri/csv/test/MainTest.java
@@ -2,6 +2,10 @@ package me.mneri.csv.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import me.mneri.csv.CsvException;
 import me.mneri.csv.CsvReader;
@@ -37,6 +41,40 @@ public class MainTest {
             try (CsvReader<Person> reader = CsvReader.open(tempFile, new PersonConverter())) {
                 Person person = reader.readLine();
                 Assert.assertEquals(mneri, person);
+            }
+        } catch (CsvException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            //@formatter:off
+            try { tempFile.delete(); } catch (Exception ignored) { }
+            //@formatter:on
+        }
+    }
+
+    @Test
+    public void stream() {
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Person person = new Person();
+            person.setFirstName("First name " + i);
+            person.setLastName("Last name " + i);
+            persons.add(person);
+        }
+
+        File tempFile = null;
+
+        try {
+            File dir = new File(System.getProperty("java.io.tmpdir"));
+            tempFile = File.createTempFile("junit_", ".csv", dir);
+
+            try (CsvWriter<Person> writer = CsvWriter.open(tempFile, new PersonConverter())) {
+                writer.writeLines(persons);
+            }
+
+            try (Stream<Person> stream = CsvReader.stream(tempFile, new PersonConverter())) {
+                List<Person> collected = stream.collect(Collectors.toList());
+                Assert.assertEquals(persons, collected);
             }
         } catch (CsvException | IOException e) {
             e.printStackTrace();
