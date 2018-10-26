d74c2fae5e2156438271cf827e49dcacfdddcf2d
Massimo Neri <hello@mneri.me>
2018-10-26 23:38:08 +0200

    Tests

--- src/main/java/me/mneri/csv/TooManyFieldsException.java
+++
@@ -1,7 +0,0 @@
-package me.mneri.csv;
-
-public class TooManyFieldsException extends IllegalCsvFormatException {
-    TooManyFieldsException(int line, int expected, int found) {
-        super(line, String.format("too many fields (expected %d, found %d).", expected, found));
-    }
-}
