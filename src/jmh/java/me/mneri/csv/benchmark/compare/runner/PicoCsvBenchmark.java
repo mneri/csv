d846e7b6f001b73a03e4cb5e0375e0dcef8dcc11
Massimo Neri <hello@mneri.me>
2026-09-24 15:35:43 +0100

    Benchmarks

--- src/jmh/java/me/mneri/csv/benchmark/compare/runner/PicoCsvBenchmark.java
+++ src/jmh/java/me/mneri/csv/benchmark/compare/runner/PicoCsvBenchmark.java
@@ -8,8 +8,7 @@ import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
@@ -24,16 +23,21 @@ public class PicoCsvBenchmark {
 
     @Benchmark
     public void run(BenchmarkState state, Blackhole bh) throws IOException {
-        List<String> fields = new ArrayList<>();
+        String[] buffer = new String[16]; // Grows if a line has more fields; reused across lines
 
         try (Csv.Reader reader = Csv.Reader.of(Csv.Format.RFC4180, OPTIONS,
                 new FileReader(state.file(), state.charset()))) {
             while (reader.readLine()) {
-                fields.clear();
+                int count = 0;
                 while (reader.readField()) {
-                    fields.add(reader.toString());
+                    // picocsv can't infer the number of fields. To keep things as fair as possible, we collect the
+                    // fields in a growing array, and we create a new final array per row.
+                    if (count == buffer.length) {
+                        buffer = Arrays.copyOf(buffer, count << 1);
+                    }
+                    buffer[count++] = reader.toString();
                 }
-                bh.consume(fields); // No domain translation, picocsv is already paying for an ArrayList
+                bh.consume(Arrays.copyOf(buffer, count));
             }
         }
     }
