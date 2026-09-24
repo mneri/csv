8a5517d9275c4ddd457b0f23c6577d002934a4ab
Massimo Neri <hello@mneri.me>
2026-09-24 15:56:33 +0100

    Benchmarks

---
+++ src/jmh/java/me/mneri/csv/benchmark/compare/runner/QuickCsvStreamerBenchmark.java
@@ -0,0 +1,50 @@
+package me.mneri.csv.benchmark.compare.runner;
+
+import me.mneri.csv.benchmark.compare.BenchmarkConstants;
+import me.mneri.csv.benchmark.compare.BenchmarkState;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import uk.elementarysoftware.quickcsv.api.CSVParser;
+import uk.elementarysoftware.quickcsv.api.CSVParserBuilder;
+import uk.elementarysoftware.quickcsv.api.CSVRecord;
+import uk.elementarysoftware.quickcsv.api.Field;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@BenchmarkMode(Mode.AverageTime)
+@Fork(BenchmarkConstants.FORKS)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = BenchmarkConstants.WARMUP_ITERATIONS)
+@Measurement(iterations = BenchmarkConstants.MEASUREMENT_ITERATIONS)
+public class QuickCsvStreamerBenchmark {
+    @Benchmark
+    public void run(BenchmarkState state, Blackhole bh) throws IOException {
+        CSVParser<String[]> parser = CSVParserBuilder.<String[], NoHeader>aParser(QuickCsvStreamerBenchmark::toDomain)
+                .forRfc4180()
+                .usingCharset(state.charset())
+                .build();
+
+        try (Stream<String[]> rows = parser.parse(state.file()).sequential()) {
+            rows.forEach(bh::consume);
+        }
+    }
+
+    private static String[] toDomain(CSVRecord record) {
+        String[] buffer = new String[16];
+        int count = 0;
+        Field field;
+        while ((field = record.getNextField()) != null) {
+            if (count == buffer.length) {
+                buffer = Arrays.copyOf(buffer, count << 1);
+            }
+            buffer[count++] = field.asString();
+        }
+        return Arrays.copyOf(buffer, count);
+    }
+
+    private enum NoHeader {
+    }
+}
